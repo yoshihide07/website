87c4094b786d7f9a776c7650b48f68ae693d2996
Yoshihide <67293958+yoshihide07@users.noreply.github.com>
2022-07-28 10:09:19 +0200

    add resource

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+    import svelteLogo from './assets/svelte.svg'
+    import css from './app.css'
+  </script>
+  
+  <main>
+    <div id="content">
+      <div id="title" class="title">
+          <h1 class="works"> Yoshihide's Website</h1>
+          <img class="logo" src="./src/.icons/yoshihide_logo.svg"  alt="Just mine logo XDD"  />
+   
+          <div id="menubar" class="menu">
+                <a href="https://yoshihide07.github.io/blog">Weblog</a>
+                <a href="https://github.com/yoshihide07">Github</a>
+                <a href="https://discord.com/invite/9hrT435cRY">Discord</a>
+              </div>
+      
+      </div>
+      <div id="content" class="content">
+          <h2 class="title_of_content"> About me</h2>
+          <article class="introducion"> <p class="article">Hi, I'm Yoshihide. Nice to see you here ;p. I'm web , python developer. Also I have expierience with adminstrate  Linux servers ( Ubuntu) I'm keen on IT studies since 2020 ( about 2 years ). Right now, I going to  fiftieth comprehensive school of Ruy Barbossa in Warsaw (division of geography-it studies-mathematics ) My other hobby are : 
+            <ul><li> History studies: '( Especially 90s of 20th century chapter)</li><li>Languages: Polish (Native speaker)' , English: (C1 Advanced) ,Russian (begginer )</li></ul></article>
+      </div>
+  </div>
+  </main>
+  
